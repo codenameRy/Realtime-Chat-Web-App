86fe38faa4dd809e2f2ebd128db1d4fa5b25e915
Ryan Lee <ryanm.lee11@gmail.com>
2020-06-16 13:27:45 -0400

    Update to front end styling and InfoBar

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client'
+import InfoBar from '../InfoBar/InfoBar'
 
 //Styling for component
 import './Chat.css';
@@ -11,10 +12,12 @@ let socket;
 const Chat = ({ location }) => {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
+    const [message, setMessage] = useState(''); //Way to store one message 
+    const [messages, setMessages] = useState([]); //Array to store all message
     const EndPoint = 'localhost:5000';
 
-//UseEffect and Cleanup instead of the class based components ComponentDidMount() and ComponentWillUnmount() 
-//to update when users log in and out
+/*UseEffect and Cleanup instead of the class based components ComponentDidMount() and ComponentWillUnmount() 
+to update when users log in and out*/
     useEffect(() => {
         const { name, room } = queryString.parse(location.search);
 
@@ -34,11 +37,40 @@ const Chat = ({ location }) => {
             socket.off()
         }
         //Specify when the useeffect is being called by using array
-    }, [EndPoint, location.search])
+    }, [EndPoint, location.search]);
 
+//UseEfect to manage messages
+ useEffect(() => {
+     socket.on('message', (message) => {
+        //Adding new message to messages array
+        setMessages([...messages, message]);
+     })
+ }, [messages]);
+
+ //Function for sending messages
+ const sendMessage = (event) => {
+     //Prevent full refresh of the keypress
+     event.preventDefault();
+
+    //Clear message field after text message has been sent
+    if(message) {
+         socket.emit('send message', message, () => setMessage(''));
+     }
+ }
+
+ console.log(message, messages)
 
     return (
-        <h1>Chat</h1>
+        <div className="outerContainer">
+        <div className="container">
+        <InfoBar room={room} />
+            {/* <input value = {message} 
+            onChange = {(event) => setMessage(event.target.value)}
+            onKeyPress={event => event.key === 'Enter' ? sendMessage(event): null } 
+            /> */}
+        </div>
+        
+      </div>
     )
 }
 
